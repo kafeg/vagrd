6cfa9d19827e0fc1bd801431ee9d557cef66f30d
Виталий Петров <petrov@nadir.kz>
2015-04-02 11:31:29 +0600

    tabulation 2

--- vagrd.sh
+++ vagrd.sh
@@ -45,64 +45,63 @@ case "$1" in
     echo "Install Meteor ramdisk"
     echo [`date +"%Y-%m-%d %H:%M"`] Start install Ramdisk >> /var/log/vagrd_sync.log
     if [ -d "/vagrant" ]; then
-	  [ -d "/home/$SUDO_USER/.meteor" ] && echo "Directory /home/$SUDO_USER/.meteor exists." || echo "Error: Directory /home/$SUDO_USER/.meteor does not exists."
-	  if [ -d "/home/$SUDO_USER/.meteor" ]; then
-	    if [ -d "/home/$SUDO_USER/.npm" ]; then
-	      if [ ! -d "/vagrant/ramdisk" ]; then
-	        echo "Backup to /home/$SUDO_USER/vagrant_backup.tar.gz"
-	        tar -zcvf /home/$SUDO_USER/vagrant_backup.tar.gz /vagrant
-	        tar -zcvf /home/$SUDO_USER/.meteor_files_backup.tar.gz /home/$SUDO_USER/.npm /home/$SUDO_USER/.meteor
-    		echo "Move folders /home/$SUDO_USER/.npm to /home/$SUDO_USER/.npm_persistent and /home/$SUDO_USER/.meteor to ~/.meteor_persistent"
-                mv /home/$SUDO_USER/.npm /home/$SUDO_USER/.npm_persistent
-                mv /home/$SUDO_USER/.meteor /home/$SUDO_USER/.meteor_persistent
+      if [ -d "/home/$SUDO_USER/.meteor" ]; then
+	if [ -d "/home/$SUDO_USER/.npm" ]; then
+	  if [ ! -d "/vagrant/ramdisk" ]; then
+	    echo "Backup to /home/$SUDO_USER/vagrant_backup.tar.gz"
+	    tar -zcvf /home/$SUDO_USER/vagrant_backup.tar.gz /vagrant
+	    tar -zcvf /home/$SUDO_USER/.meteor_files_backup.tar.gz /home/$SUDO_USER/.npm /home/$SUDO_USER/.meteor
+    	    echo "Move folders /home/$SUDO_USER/.npm to /home/$SUDO_USER/.npm_persistent and /home/$SUDO_USER/.meteor to ~/.meteor_persistent"
+            mv /home/$SUDO_USER/.npm /home/$SUDO_USER/.npm_persistent
+            mv /home/$SUDO_USER/.meteor /home/$SUDO_USER/.meteor_persistent
 
-		echo "Make folders /home/$SUDO_USER/.npm and /home/$SUDO_USER/.meteor"
-		mkdir /home/$SUDO_USER/.npm
-		mkdir /home/$SUDO_USER/.meteor
+	    echo "Make folders /home/$SUDO_USER/.npm and /home/$SUDO_USER/.meteor"
+	    mkdir /home/$SUDO_USER/.npm
+	    mkdir /home/$SUDO_USER/.meteor
 
-		echo "Copy script to /etc/init.d/vagrd"
-	        cp ${0} /etc/init.d/vagrd
-		sed -i "s/USERNAME=\"notfound\"/USERNAME=\"$SUDO_USER\"/g" /etc/init.d/vagrd
-                chmod a+x /etc/init.d/vagrd
+	    echo "Copy script to /etc/init.d/vagrd"
+	    cp ${0} /etc/init.d/vagrd
+	    sed -i "s/USERNAME=\"notfound\"/USERNAME=\"$SUDO_USER\"/g" /etc/init.d/vagrd
+            chmod a+x /etc/init.d/vagrd
 
-	        echo "Create folder /vagrant/ramdisk"
-	        mkdir /vagrant/ramdisk
-	        mkdir /vagrant/ramproj
-	        echo 'test' > /vagrant/ramproj/test.txt
+	    echo "Create folder /vagrant/ramdisk"
+	    mkdir /vagrant/ramdisk
+	    mkdir /vagrant/ramproj
+	    echo 'test' > /vagrant/ramproj/test.txt
 
-	        echo "Create tmpfs in fstab"
-	        echo "tmpfs /vagrant/ramdisk tmpfs      defaults,noatime,mode=077 0 0" >> /etc/fstab
-    	        echo "tmpfs /home/$SUDO_USER/.meteor	tmpfs	defaults,noatime,mode=077 0 0" >> /etc/fstab
-	        echo "tmpfs /home/$SUDO_USER/.npm tmpfs      defaults,noatime,mode=077 0 0" >> /etc/fstab
+	    echo "Create tmpfs in fstab"
+	    echo "tmpfs /vagrant/ramdisk tmpfs      defaults,noatime,mode=077 0 0" >> /etc/fstab
+    	    echo "tmpfs /home/$SUDO_USER/.meteor	tmpfs	defaults,noatime,mode=077 0 0" >> /etc/fstab
+	    echo "tmpfs /home/$SUDO_USER/.npm tmpfs      defaults,noatime,mode=077 0 0" >> /etc/fstab
 
-	        echo "Mounting /vagrant/ramdisk, /home/$SUDO_USER/.meteor and /home/$SUDO_USER/.npm"
-	        mount /vagrant/ramdisk
-	        mount /home/$SUDO_USER/.npm
-	        mount /home/$SUDO_USER/.meteor
+	    echo "Mounting /vagrant/ramdisk, /home/$SUDO_USER/.meteor and /home/$SUDO_USER/.npm"
+	    mount /vagrant/ramdisk
+	    mount /home/$SUDO_USER/.npm
+	    mount /home/$SUDO_USER/.meteor
 
-	        echo "Install service vagrd"
-		update-rc.d vagrd defaults 00 99
-		echo `ls /vagrant/ramdisk`
+	    echo "Install service vagrd"
+	    update-rc.d vagrd defaults 00 99
+	    echo `ls /vagrant/ramdisk`
 
-		echo "Install cron sync task"
-		echo "2 * * * * root        service vagrd sync >> /dev/null 2>&1" >> /etc/crontab
-		echo "Installation complete! Run 'sudo service vagrd start'"
-	      else
-	        echo "Script already installed! Exit..."
-	        echo [`date +"%Y-%m-%d %H:%M"`] Script already installed! >> /var/log/vagrd_sync.log
-	      fi
+	    echo "Install cron sync task"
+	    echo "2 * * * * root        service vagrd sync >> /dev/null 2>&1" >> /etc/crontab
+	    echo "Installation complete! Run 'sudo service vagrd start'"
+          else
+            echo "Script already installed! Exit..."
+            echo [`date +"%Y-%m-%d %H:%M"`] Script already installed! >> /var/log/vagrd_sync.log
+          fi
         else
           echo "Npm not found! Exit..."
           echo [`date +"%Y-%m-%d %H:%M"`] Npm not found! >> /var/log/vagrd_sync.log
         fi
-	  else
-	    echo "Meteor not found! Exit..."
-	    echo [`date +"%Y-%m-%d %H:%M"`] Meteor not found! >> /var/log/vagrd_sync.log
-	  fi
-	else
-	  echo "Vagrant dir not found! Exit..."
-	  echo [`date +"%Y-%m-%d %H:%M"`] Vagrant not found! >> /var/log/vagrd_sync.log
-	fi
+      else
+        echo "Meteor not found! Exit..."
+        echo [`date +"%Y-%m-%d %H:%M"`] Meteor not found! >> /var/log/vagrd_sync.log
+      fi
+    else
+      echo "Vagrant dir not found! Exit..."
+      echo [`date +"%Y-%m-%d %H:%M"`] Vagrant not found! >> /var/log/vagrd_sync.log
+    fi
     ;;
   uninstall)
     if [ -d "/vagrant/ramdisk" ]; then
@@ -110,25 +109,31 @@ case "$1" in
       echo [`date +"%Y-%m-%d %H:%M"`] Start uninstall Ramdisk >> /var/log/vagrd_sync.log
       echo "Stop service"
       service vagrd stop
+
       echo "Unount tmpfs"
       umount /vagrant/ramdisk
       umount /home/$SUDO_USER/.npm
       umount /home/$SUDO_USER/.meteor
+
       echo "Remove dirs"
       rmdir /vagrant/ramdisk
       rmdir /home/$SUDO_USER/.npm
       rmdir /home/$SUDO_USER/.meteor
+
       echo "Restore old dirs"
       mv /home/$SUDO_USER/.npm_persistent /home/$SUDO_USER/.npm
       mv /home/$SUDO_USER/.meteor_persistent /home/$SUDO_USER/.meteor
       rm /vagrant/ramproj/test.txt
       rmdir /vagrant/ramproj
+
       echo "Restore fstab"
       sed -i '\/vagrant\/ramdisk/d' /etc/fstab
       sed -i '\/home\/$SUDO_USER\/.meteor/d' /etc/fstab
       sed -i '\/home\/$SUDO_USER\/.npm/d' /etc/fstab
+
       echo "Restore crontab"
       sed -i '/vagrd/d' /etc/crontab
+
       echo "Remove service"
       update-rc.d -f vagrd remove
       rm /etc/init.d/vagrd
